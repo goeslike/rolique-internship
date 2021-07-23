80aa203796b29be3fdfa2c9f4278e4e603b9dc83
AndriiHrytsai <andriyhrytsay@gmail.com>
2021-07-23 13:15:01 +0300

    router, app.js

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 const { config: { PORT } } = require('./configs');
 
 const Sentry = require('./logger/sentry');
+const { apiRouter } = require('./routes');
 
 const app = express();
+
+function _mongooseConnector() {
+    mongoose.connect('впиши тут базу даних', { useNewUrlParser: true, useUnifiedTopology: true });
+}
+_mongooseConnector();
 
 app.use(Sentry.Handlers.errorHandler());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/', apiRouter);
 
 // eslint-disable-next-line no-unused-vars
 app.use('*', (err, req, res, next) => {
