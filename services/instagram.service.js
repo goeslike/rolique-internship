c2c95ef6e6c1ee6b40d76bb4e8600547592499fb
NataWhite <nata.ivanova.one@gmail.com>
2021-08-03 13:56:06 +0300

    binary data / all promises

--- services/instagram.service.js
+++ services/instagram.service.js
@@ -4,34 +4,38 @@ const { instagramApi } = require('../helpers');
 
 module.exports = {
     getImagesData: async (username) => {
-        const accountImages = [];
+        const accountImagePromises = [];
         const ig = await instagramApi.getInstagramData();
         const client = await ig.user.searchExact(username);
         if (!client) {
-            return accountImages;
+            return accountImagePromises;
         }
         const clientFeed = await ig.feed.user(
             client.pk
         );
         const page = await clientFeed.items();
         for (const post of page) {
-            if (accountImages.length < 12) {
+            if (accountImagePromises.length < 12) {
                 if (post.carousel_media) {
                     for (const item of post.carousel_media) {
-                        const filePromise = fetch(item.image_versions2.candidates[0].url)
-                            .then((data) => data.blob());
-                        const file = await filePromise;
-                        accountImages.push({ image: file });
+                        accountImagePromises.push({ imageUrl: item.image_versions2.candidates[0].url });
                     }
                 }
                 if (post.image_versions2) {
-                    const filePromise = fetch(post.image_versions2.candidates[0].url)
-                        .then((data) => data.blob());
-                    const file = await filePromise;
-                    accountImages.push({ image: file });
+                    accountImagePromises.push({ imageUrl: post.image_versions2.candidates[0].url });
                 }
             }
         }
-        return accountImages;
+        const arrayPromiseFile = [];
+
+        for (const promise of accountImagePromises) {
+            const filePromise = fetch(promise.imageUrl)
+                .then((data) => data.blob());
+            arrayPromiseFile.push(filePromise);
+        }
+
+        await Promise.all(arrayPromiseFile);
+
+        return arrayPromiseFile;
     }
 };
