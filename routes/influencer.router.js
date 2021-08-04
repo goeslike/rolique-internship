7a7aede6fa4fc721563dd0558ff764ac6f66db70
Ann <kharko2112.mechmat@gmail.com>
2021-08-04 20:12:23 +0300

    update+

--- routes/influencer.router.js
+++ routes/influencer.router.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
 
 const {
-    accessMiddleware,
-    authMiddleware,
-    checkIdValid,
-    dynamicMiddleware,
+    // accessMiddleware,
+    // authMiddleware,
+    // checkIdValid,
+    // dynamicMiddleware,
     imageMiddleware
 } = require('../middlewares');
-const { ADMIN, MANAGER } = require('../constants/roles.enum');
-const { CREATE, UPDATE } = require('../constants/action.enum');
+// const { ADMIN, MANAGER } = require('../constants/roles.enum');
+// const { CREATE, UPDATE } = require('../constants/action.enum');
 const { influencerController } = require('../controllers');
-const { influencerValidator } = require('../validators');
+// const { influencerValidator } = require('../validators');
 
 router.post('/',
     // authMiddleware.checkAccessToken,
