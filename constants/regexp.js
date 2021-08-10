a842d5de57c64854ec55ed255bba5eb442080919
AndriiHrytsai <andriyhrytsay@gmail.com>
2021-08-10 13:01:50 +0300

    fix regular expressions

--- constants/regexp.js
+++ constants/regexp.js
@@ -1,5 +1,6 @@
 module.exports = {
     EMAIL_REGEXP: new RegExp(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/),
-    PASSWORD_REGEXP: new RegExp(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/),
-    PHONE_REGEXP: new RegExp('^[+]*[0-9]{5,20}$')
+    PASSWORD_REGEXP: new RegExp(/^(?=.*?[a-z])(?=.*?[0-9]).{8,}$/),
+    // PHONE_REGEXP: new RegExp('^[+]*[0-9]{5,20}$')
+    PHONE_REGEXP: new RegExp('^(s*)?(/+)?([- _():=+]?d[- _():=+]?){10,14}(s*)?')
 };
